a861a59c6f1086ef0377b8b877f834682ebc56fb
Lachlan Musicman <datakid@gmail.com>
2015-04-04 11:47:11 +1100

    fixed the settings

--- df-site/df/settings/base.py
+++ df-site/df/settings/base.py
@@ -71,21 +71,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'df.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mydatabase',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
-        'HOST': '127.0.0.1',
-        'PORT': '5432', 
-        }
-}
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
